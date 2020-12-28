dd4878bc053588880b1e03e7e2bdff54681784b8
umarnaeem1 <umarnaeem01@gmail.com>
2020-12-29 00:11:11 +0500

    Make compatible for WordPress 5.6

--- 99robots-custom-image-sizes.php
+++ 99robots-custom-image-sizes.php
@@ -3,7 +3,7 @@
  * Plugin Name:	Custom Image Sizes by 99 Robots
  * Plugin URI:	https://draftpress.com/products/?utm_source=plugin-uri&utm_medium=custom-image-sizes&utm_campaign=plugins-page
  * Description:	Custom Image Sizes by 99 Robots is a quick and simple way for you to add your own image sizes to your WordPress site.
- * Version: 1.2.5
+ * Version: 1.2.6
  * Author: 99 Robots
  * Author URI: https://draftpress.com
  * License: GPL2
@@ -26,7 +26,7 @@ class NNR_Custom_Image_Sizes {
 	 * NNR_Custom_Image_Sizes version.
 	 * @var string
 	 */
-	public $version = '1.2.5';
+	public $version = '1.2.6';
 
 	/**
 	 * The single instance of the class.
