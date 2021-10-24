32f69c08558208308ad426b90c2bbb72349f7c60
Alex Lion <learnwithalex@gmail.com>
2021-10-24 01:05:40 -0700

    I18N Issues
    - Plugin's text domain must be equal to its owned slug.
    - I modified all I18N related code.
    - Make the string `Close` and `Subscribe` localizabe.

--- views/header.php
+++ views/header.php
@@ -4,12 +4,12 @@
 	<div class="nnr-logo"></div>
 
 	<div class="nnr-product-details">
-		<span class="nnr-product-name"><?php esc_html_e( 'Custom Image Sizes', '99robots-custom-image-sizes' ) ?></span>
+		<span class="nnr-product-name"><?php esc_html_e( 'Custom Image Sizes', 'custom-image-sizes-by-99-robots' ) ?></span>
 		<span class="nnr-product-version"><?php echo wps_custom_sizes()->get_version() ?></span>
 	</div>
 
 	<a href="http://draftpress.com/products" target="_blank">
-		<button class="nnr-header-button pull-right"><?php esc_html_e( 'More Products', '99robots-custom-image-sizes' ) ?></button>
+		<button class="nnr-header-button pull-right"><?php esc_html_e( 'More Products', 'custom-image-sizes-by-99-robots' ) ?></button>
 	</a>
 
 </div>
--- views/sidebar.php
+++ views/sidebar.php
@@ -3,7 +3,7 @@
 	<div class="panel panel-default">
 
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Signup now to get notified of plugin updates, awesome themes, and more. Over 10,000+ already have:', '99robots-custom-image-sizes' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Signup now to get notified of plugin updates, awesome themes, and more. Over 10,000+ already have:', 'custom-image-sizes-by-99-robots' ) ?></h3>
 		</div>
 
 		<div class="panel-body">
@@ -23,14 +23,14 @@
 			<form action="//wpsite.us5.list-manage.com/subscribe/post?u=82c2341134bbdc37714642adb&amp;id=642b18616e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate style="padding-left: 0;">
 				<div id="mc_embed_signup_scroll">
 					<div style="margin-bottom: 20px;">
-						<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="<?php esc_html_e( 'Email Address', '99robots-custom-image-sizes' ) ?>">
+						<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="<?php esc_html_e( 'Email Address', 'custom-image-sizes-by-99-robots' ) ?>">
 					</div>
 					<div id="mce-responses" class="clear">
 						<div class="response" id="mce-error-response" style="display:none"></div>
 						<div class="response" id="mce-success-response" style="display:none"></div>
 					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					<div style="position: absolute; left: -5000px;"><input type="text" name="b_82c2341134bbdc37714642adb_642b18616e" tabindex="-1" value=""></div>
-					<div><input style="width:100%;" type="submit" value="Subscribe" name="subscribe" class="btn btn-default"></div>
+					<div><input style="width:100%;" type="submit" value="<?php esc_attr_e( 'Subscribe', 'custom-image-sizes-by-99-robots' ); ?>" name="subscribe" class="btn btn-default"></div>
 				</div>
 			</form>
 			</div>
@@ -42,12 +42,12 @@
 	<div class="panel panel-default">
 
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Must-Read Articles', '99robots-custom-image-sizes' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Must-Read Articles', 'custom-image-sizes-by-99-robots' ) ?></h3>
 		</div>
 
 		<div class="panel-body">
 			<div class="wpsite_feed">
-				<script src="http://feeds.feedburner.com/99robots?format=sigpro" type="text/javascript" ></script><noscript><p><?php esc_html_e( 'Subscribe to 99 Robots Feed:', '99robots-custom-image-sizes' ) ?> <a href="http://feeds.feedburner.com/99robots"></a><br/><?php esc_html_e( 'Powered by FeedBurner', '99robots-custom-image-sizes' ) ?></p> </noscript>
+				<script src="http://feeds.feedburner.com/99robots?format=sigpro" type="text/javascript" ></script><noscript><p><?php esc_html_e( 'Subscribe to 99 Robots Feed:', 'custom-image-sizes-by-99-robots' ) ?> <a href="http://feeds.feedburner.com/99robots"></a><br/><?php esc_html_e( 'Powered by FeedBurner', 'custom-image-sizes-by-99-robots' ) ?></p> </noscript>
 			</div>
 		</div>
 
@@ -56,7 +56,7 @@
 	<div class="panel panel-default">
 
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Need a Plugin or Theme Developed?', '99robots-custom-image-sizes' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Need a Plugin or Theme Developed?', 'custom-image-sizes-by-99-robots' ) ?></h3>
 		</div>
 
 		<div class="panel-body">
